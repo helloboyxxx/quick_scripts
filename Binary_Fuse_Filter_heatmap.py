6a0dd033066909eefa516db48da39c6006d05da5
helloboyxxx <51398343+helloboyxxx@users.noreply.github.com>
2024-06-17 17:41:42 -0500

    Update Binary_Fuse_Filter_heatmap.py

--- Binary_Fuse_Filter_heatmap.py
+++ Binary_Fuse_Filter_heatmap.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-N = 1000  # Length of the array
-w = 100    # Window size
-k = 10000  # Number of iterations
+N = 5000  # Length of the array
+w = 1000    # Window size
+k = 50000  # Number of iterations
 
 d = 4
 
@@ -14,12 +14,13 @@ array = np.zeros(N)
 # Perform the iterations
 for _ in range(k):
     # Uniformly randomly choose the window location
-    window_start = np.random.randint(0, N - w + 1)
+    window_start = np.random.randint(0, int(N/w)) * w   # ChalametPIR paper pseudocode
+    # window_start = np.random.randint(0, N - w + 1)      # my understanding of BFF window selection
     
-    # Within the window, randomly choose three positions
-    positions = np.random.choice(range(window_start, window_start + w), d, replace=False)
+    # Within the window, randomly choose d many positions (simulating hashing)
+    positions = np.random.choice(range(window_start, window_start + w), d, replace=True)
     
-    # Add 1 to each chosen position
+    # Add 1 to each chosen position (counting frequency)
     for pos in positions:
         array[pos] += 1
 
@@ -34,3 +35,7 @@ plt.show()
 
 
 # The results shows that the two ends has lower frequency than the middle part.
+
+
+
+
